348ce6374cb6afbd6b9dc84f780eb20376dd978b
Said Alhinai <39749832+Crash619@users.noreply.github.com>
2020-01-15 12:54:20 +0400

    Add files via upload

---
+++ Mad Lips Game/Mad Lips Game/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mad_Lips_Game
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String Color, PluralNoun, Food;
+
+            Console.WriteLine("Plese Choose a color: ");
+            Color = Console.ReadLine();
+
+            Console.WriteLine("Plese Choose a Plural Noun: ");
+            PluralNoun = Console.ReadLine();
+
+            Console.WriteLine("Plese Choose a food: ");
+            Food = Console.ReadLine();
+
+            Console.WriteLine("Roses are "+Color);
+            Console.WriteLine( PluralNoun+" are Blue");
+            Console.WriteLine("I eat "+Food);
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Mad Lips Game/Mad Lips Game/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Mad Lips Game")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Mad Lips Game")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("86215e0a-813b-4508-984b-6cf083f238d7")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
