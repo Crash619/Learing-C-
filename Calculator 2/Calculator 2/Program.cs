59a19ef0b44746bd6db353c52dfdb2dc8d39391b
Said Alhinai <39749832+Crash619@users.noreply.github.com>
2020-01-19 10:09:57 +0400

    Add files via upload

---
+++ Calculator 2/Calculator 2/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please Enter a Number:");
+            double num1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine(" Please Chose and Operator: ");
+            string OP = Console.ReadLine();
+
+            Console.WriteLine("Please Enter  your Second Number:");
+            double num2 = Convert.ToDouble(Console.ReadLine());
+             if (OP == "+"){
+                Console.WriteLine(num1 + num2);
+            }
+             else if( OP == "*")
+            {
+                Console.WriteLine(num1 * num2);
+            }
+             else if( OP == "/")
+            {
+                Console.WriteLine(num1 / num2);
+            }
+             else if( OP == "-")
+            {
+                Console.WriteLine(num1 - num2);
+            }
+            else
+            {
+                Console.WriteLine("Invalid Operator");
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ More if statment/More if statment/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace More_if_statment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(GetMax(22, 10));
+
+            Console.ReadLine();
+        }
+
+        static int GetMax(int num1, int num2)
+        {
+            int result;
+            if (num1 > num2)
+            {
+                result = num1;
+
+            }
+            else
+            {
+                result = num2;
+            }
+            return result;
+        }
+    }
+}
